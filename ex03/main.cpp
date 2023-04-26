5156cb25f892494e0065498863a03fd90537f999
รัฐ ธรรมธัชอารี <rath@raths-MacBook-Pro.local>
2023-04-26 22:49:52 +0700

    add: complete

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -2,7 +2,9 @@
 
 int main(void)
 {
- if (bsp(Point(0, 0), Point(10, 0), Point(10, 30), Point(10, 15)) == true)
+ // if (bsp(Point(0, 0), Point(10, 0), Point(10, 20), Point(10, 20.5)) == true)
+ // if (bsp(Point(0, 0), Point(10, 0), Point(10, 30), Point(10, 15)) == true)
+ if (bsp(Point(0, 0), Point(10, 0), Point(10, 20), Point(5, 2)) == true)
  {
   std::cout << "Point is in the triangle" << std::endl;
  }
